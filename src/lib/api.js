d33c14ec9fc7cc90f3000ea37adc448aa03f2591
Sam Noh <samnoh21@outlook.com>
2019-07-17 20:59:03 +1200

    fix - typo

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://yts.lt/api/v2/list_movies.json?';
 
-export const getMovies = async (limit, rating) => {
+export const getMovies = (limit, rating) => {
     let URL = '';
     if (limit > 0) URL += `&limit=${limit}`;
     if (rating > 0) URL += `&minimum_rating=${rating}`;
