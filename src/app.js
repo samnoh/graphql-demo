d888de1bae33b0ec4dc158bb4165f0b3ce7b7be1
Sam Noh <samnoh21@outlook.com>
2019-07-18 21:56:17 +1200

    add - morgan & cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import path from 'path';
+import morgan from 'morgan';
+import cors from 'cors';
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
 
@@ -11,8 +13,19 @@ const prod = process.env.NODE_ENV === 'production';
 const typeDefs = importSchema(path.join(__dirname, 'graphql/schema.graphql'));
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start({ port: process.env.PORT || 4000, playground: prod ? false : '/' }, ({ port }) =>
-    console.log(`GraphQL server running on localhost:${port}`)
+if (prod) {
+    server.express.use(morgan('combined'));
+} else {
+    server.express.use(morgan('dev'));
+}
+
+server.start(
+    {
+        port: process.env.PORT || 4000,
+        playground: prod ? false : '/',
+        cors: cors({ origin: prod ? 'https://whatever' : true, optionsSuccessStatus: 200 })
+    },
+    ({ port }) => console.log(`GraphQL server running on ${port}`)
 );
 
 connect();
